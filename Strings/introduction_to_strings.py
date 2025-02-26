e8434eb886e6b1a3b1871559729de07d37edc0d9
Ben McCarthy <ben.mccarthy92@gmail.com>
2025-02-26 23:28:27 +0000

    Start Introduction To Strings

---
+++ Strings/introduction_to_strings.py
@@ -0,0 +1,15 @@
+#Introduction To Strings
+favorite_word = "paroxysm"
+print(favorite_word)
+
+#They're All Lists!
+my_name = "Ben"
+first_initial = my_name[0]
+
+#Cut Me A Slice Of String
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+
+
+#Introduction To Strings
