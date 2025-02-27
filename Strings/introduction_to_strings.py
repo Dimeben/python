ee6d67a94d118541f117a0a3d4a48d3a3c87b1a3
ben.mccarthy <ben.mccarthy@iliad-solutions.com>
2025-02-27 09:08:38 +0000

    Work up to Escape Characters section

--- Strings/introduction_to_strings.py
+++ Strings/introduction_to_strings.py
@@ -1,15 +1,48 @@
 #Introduction To Strings
-favorite_word = "paroxysm"
-print(favorite_word)
+favorite_word = "Paroxysm"
 
 #They're All Lists!
 my_name = "Ben"
 first_initial = my_name[0]
 
 #Cut Me A Slice Of String
-first_name = "Rodrigo"
-last_name = "Villanueva"
+slice_first_name = "Rodrigo"
+slice_last_name = "Villanueva"
+#Slices the first 5 characters of last_name
+slice_new_account = slice_last_name[:5]
+#Slices the 3rd - 6th characters of last_name
+slice_temp_password = slice_last_name[2:6]
 
+#Concatenating Strings
+concat_first_name = "Julie"
+concat_last_name = "Blevins"
 
+#Takes two inputs, concatenates the first three letters of each and then returns the new account name.
+def account_generator(first_name, last_name):
+    return first_name[:3] + last_name[:3]
 
-#Introduction To Strings
+new_account = account_generator(concat_first_name, concat_last_name)
+
+#More and More String Slicing (How Long is that String?)
+more_sliced_first_name = "Reiko"
+more_sliced_last_name = "Matsuki"
+
+#Takes two inputs, concatenates the last three letters of each and then returns the new password.
+def password_generator(first_name, last_name):
+    return first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+
+temp_password = password_generator(more_sliced_first_name, more_sliced_last_name)
+
+#Negative Indices
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+second_to_last = company_motto[-2]
+final_word = company_motto[-4:]
+
+#Strings are Immutable
+first_name = "Bob"
+last_name = "Daily"
+fixed_first_name = "R" + first_name[1:]
+
+#Escape Characters
+password = "theycallme\"crazy\"91"
