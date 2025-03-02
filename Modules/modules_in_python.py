331b33c6fca708b8b47299e7c92f6566dc10e97b
Ben McCarthy <ben.mccarthy92@gmail.com>
2025-03-02 09:32:37 +0000

    Work up to Time Traveler's Toolkit

--- Modules/modules_in_python.py
+++ Modules/modules_in_python.py
@@ -1,12 +1,24 @@
-# Import datetime from datetime below:
+"""Modules"""
+
 from datetime import datetime
 import random
+from decimal import Decimal
 
+# Sets variable as the current date & time in the following format: 2025-03-01 07:23:03.520673
 current_time = datetime.now()
-print(current_time)
 
+# randint() takes 2 arguments and will pick a random value within that range
 random_list = [random.randint(1, 100) for i in range(101)]
-print(random_list)
-
+# choice() takes an argument and will pick a random element within that range
 randomer_number = random.choice(random_list)
-print(randomer_number)
+
+numbers_a = range(1, 13)
+# sample() takes 2 arguments (sequence, k) and returns a k length new list of elements chosen from the sequence
+numbers_b = random.sample(range(1000), 12)
+
+# Fix the floating point math below:
+two_decimal_points = Decimal("0.2") + Decimal("0.69")
+print(two_decimal_points)
+
+four_decimal_points = Decimal("0.53") * Decimal("0.65")
+print(four_decimal_points)
